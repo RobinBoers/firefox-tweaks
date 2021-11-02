a532781dd690c469950ce0c2908598f23da81c46
Robin Boers <robindev2019@outlook.com>
2021-11-02 17:32:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# :fox_face: Firefox tweaks
+I dont like the new Proton design, so this is my way of tweaking the Firefox UI using custom userChrome.css stylesheets.
+
+Tested on Firefox 90-91
+
---
+++ user.js
@@ -0,0 +1,17 @@
+// ** Theme Related Options ****************************************************
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.proton.enabled", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.backdrop-filter.enabled", true);
+user_pref("browser.compactmode.show", true);
+
+// about:home Search Bar
+user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", true);
+
+// ** Useful Options ***********************************************************
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
+
+// Integrated unit convertor at urlbar
+// user_pref("browser.urlbar.unitConversion.enabled", true);
