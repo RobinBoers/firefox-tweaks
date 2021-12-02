04a38f89f5defc6a7cc3898478ba566bde13414a
Robin Boers <robindev2019@outlook.com>
2021-12-02 14:13:20 +0100

    update some stuff

--- user.js
+++ user.js
@@ -1,17 +1,110 @@
 // ** Theme Related Options ****************************************************
-// userchrome.css usercontent.css activate
+
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.proton.enabled", true);
 user_pref("svg.context-properties.content.enabled", true);
+user_pref("mozilla.widget.use-argb-visuals", true);
 user_pref("layout.css.backdrop-filter.enabled", true);
 user_pref("browser.compactmode.show", true);
 
-// about:home Search Bar
+// ** New Tab ******************************************************************
+
 user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", true);
 
-// ** Useful Options ***********************************************************
-// Integrated calculator at urlbar
-user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
 
-// Integrated unit convertor at urlbar
-// user_pref("browser.urlbar.unitConversion.enabled", true);
+// ** Useful Options ***********************************************************
+
+user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.urlbar.unitConversion.enabled", true);
+
+user_pref("accessibility.typeaheadfind.flashBar", 0);
+user_pref("browser.download.lastDir", "/home/robin/Downloads");
+
+// ** Tabs *********************************************************************
+
+user_pref("browser.tabs.tabClipWidth", 83);
+user_pref("browser.tabs.tabMinWidth", 112);
+user_pref("browser.theme.toolbar-theme", 0);
+
+// ** DevTools *****************************************************************
+
+user_pref("devtools.cache.disabled", true);
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
+user_pref("devtools.everOpened", true);
+user_pref("devtools.toolbox.tabsOrder", "inspector,webconsole,netmonitor,jsdebugger,styleeditor,performance,memory,storage,application,accessibility");
+
+// ** Browser features *********************************************************
+
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.screenshots.disabled", true);
+
+user_pref("browser.toolbars.bookmarks.showOtherBookmarks", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+
+user_pref("browser.quitShortcut.disabled", true);
+user_pref("browser.tabs.drawInTitlebar", false); // Set this to true outside tiling wm
+user_pref("browser.tabs.loadInBackground", false);
+
+// ** Scrolling ****************************************************************
+
+user_pref("general.smoothScroll.currentVelocityWeighting", 0.12);
+user_pref("general.smoothScroll.durationToIntervalRatio", 1000);
+user_pref("general.smoothScroll.lines.durationMaxMS", 100);
+user_pref("general.smoothScroll.lines.durationMinMS", 0);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 100);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 0);
+user_pref("general.smoothScroll.mouseWheel.migrationPercent", 100);
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 200);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 200);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 10);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 1.20);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 1000);
+user_pref("general.smoothScroll.other.durationMaxMS", 100);
+user_pref("general.smoothScroll.other.durationMinMS", 0);
+user_pref("general.smoothScroll.pages.durationMaxMS", 100);
+user_pref("general.smoothScroll.pages.durationMinMS", 0);
+user_pref("general.smoothScroll.pixels.durationMaxMS", 100);
+user_pref("general.smoothScroll.pixels.durationMinMS", 0);
+user_pref("general.smoothScroll.scrollbars.durationMaxMS", 100);
+user_pref("general.smoothScroll.scrollbars.durationMinMS", 0);
+user_pref("general.smoothScroll.stopDecelerationWeighting", 0.6);
+
+user_pref("mousewheel.acceleration.factor", 3);
+user_pref("mousewheel.acceleration.start", -1);
+user_pref("mousewheel.default.delta_multiplier_x", 100);
+user_pref("mousewheel.default.delta_multiplier_y", 100);
+user_pref("mousewheel.default.delta_multiplier_z", 100);
+user_pref("mousewheel.min_line_scroll_amount", 0);
+
+user_pref("mousewheel.system_scroll_override.enabled", true);
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled", false);
+
+// ** URL bar ******************************************************************
+
+user_pref("browser.urlbar.placeholderName", "Google");
+user_pref("browser.urlbar.placeholderName.private", "Google");
+user_pref("browser.urlbar.quicksuggest.scenario", "history");
+user_pref("browser.urlbar.shortcuts.bookmarks", false);
+user_pref("browser.urlbar.shortcuts.tabs", false);
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+
+// ** Widgets ******************************************************************
+
+user_pref("browser.uiCustomization.state", {"placements":{"widget-overflow-fixed-list":["_46551ec9-40f0-4e47-8e18-8e5cf550cfb8_-browser-action","materialfox-helper_mucksponge-browser-action","developer-button","addon_darkreader_org-browser-action","ublock0_raymondhill_net-browser-action","firefoxcolor_mozilla_com-browser-action","jid1-93cwpmrbvpjrqa_jetpack-browser-action"],"nav-bar":["back-button","sync-button","forward-button","stop-reload-button","customizableui-special-spring34","customizableui-special-spring38","urlbar-container","downloads-button","history-panelmenu","customizableui-special-spring39","customizableui-special-spring35","_testpilot-containers-browser-action","adblockultimate_adblockultimate_net-browser-action","jid1-zadieub7xozojw_jetpack-browser-action","fxa-toolbar-menu-button","canvasblocker_kkapsner_de-browser-action","newtaboverride_agenedia_com-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"seen":["developer-button","addon_darkreader_org-browser-action","enhancerforyoutube_maximerf_addons_mozilla_org-browser-action","_contain-facebook-browser-action","_d04b0b40-3dab-4f0b-97a6-04ec3eddbfb0_-browser-action","adblockultimate_adblockultimate_net-browser-action","_46551ec9-40f0-4e47-8e18-8e5cf550cfb8_-browser-action","save-to-pocket-button","_testpilot-containers-browser-action","jid1-zadieub7xozojw_jetpack-browser-action","_d07ccf11-c0cd-4938-a265-2a4d6ad01189_-browser-action","ublock0_raymondhill_net-browser-action","canvasblocker_kkapsner_de-browser-action","firefoxcolor_mozilla_com-browser-action","materialfox-helper_mucksponge-browser-action","_e7476172-097c-4b77-b56e-f56a894adca9_-browser-action","markdown-here-webext_adam_pritchard-browser-action","_85860b32-02a8-431a-b2b1-40fbd64c9c69_-browser-action","newtaboverride_agenedia_com-browser-action","jid1-93cwpmrbvpjrqa_jetpack-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","widget-overflow-fixed-list","TabsToolbar","toolbar-menubar"],"currentVersion":17,"newElementCount":49});
+
+// ** Other *********************************************************************
+
+user_pref("layers.async-pan-zoom.enabled", false);
+user_pref("layout.css.backdrop-filter.enabled", true);
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("layout.css.scroll-behavior.spring-constant", 250.0);
